c5233ca665ecc3cd682abffc2bcded0f84a35543
Thomas Weber <weber@openinnovator.net>
2016-01-16 13:43:33 +0100

    Added engine usage info

--- src/main/scala/threesixty/engine/VisualizationEngine.scala
+++ src/main/scala/threesixty/engine/VisualizationEngine.scala
@@ -17,7 +17,43 @@ case class VisualizationEngine(
     processor: Processor,
     visualizer: Visualizer,
     dbAdapter: DatabaseAdapter
-) extends Engine with HttpRequestProcessor {
+) extends Engine with HttpRequestProcessor with UsageInfo {
+
+    def usage: String =
+"""Threesixty - Visualization Engine
+
+HELP
+{                                       Returns usage information for modules of the engine.
+    "type": "help",                     HELP_KEYWORD can be one of
+    "for": HELP_KEYWORD                   - "visualizations"     Lists all available visualizations
+}                                         - "processingmethods"  Lists all available processing methods
+                                          - visualization_name   Shows arguments for this visualization
+                                          - processing_method    Shows arguments for this processing method
+
+VISUALIZATION
+{                                       Returns a visualization as SVG
+    "type": "visualization",
+    "data": [ DATA_IDS ],               DATA_IDS is a list of IDs of the datasets for processing
+    "visualization": {                  The "visualization" parameter is optional. If none is given it is deduced form the data.
+        "type": VISUALIZATION_TYPE      VISUALIZATION_TYPE is the name of the visualization
+        "args": VISUALIZATION_ARGS      VISUALIZATION_ARGS are the arguments of the visualization
+    },
+    "processor": [ PROCESSING_STEPS]    PROCESSING_STEPS is a list of processing steps
+}                                       processing steps follow the format:
+                                        {
+                                            "method": "PROCESSING_METHOD",
+                                            "args": PROCESSING_METHOD_ARGS
+                                            "data": [ DATA_IDS ]
+                                            "idmapping": [ ID_MAPPING ]
+                                        }
+                                        PROCESSING_METHOD is the name of the method.
+                                        PROCESSING_METHOD_ARGS are the parameters for that method
+                                        DATA_IDS is a list of IDs of datasets this method is applied to
+                                        ID_MAPPING allows renaming the output dataset. Doing this allows
+                                        continue using both the original dataset unchanged and the
+                                        processed one.
+
+"""
 
     def processRequest(jsonString: String): EngineResponse = {
         val json = jsonString.parseJson.asJsObject
