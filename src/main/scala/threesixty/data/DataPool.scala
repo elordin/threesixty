52dc9238b0fa9ef0a11152028a7fd2e5394fd1c5
Thomas Weber <weber@openinnovator.net>
2016-01-27 00:24:34 +0100

    Added DataPool class

---
+++ src/main/scala/threesixty/data/DataPool.scala
@@ -0,0 +1,61 @@
+package threesixty.data
+
+import threesixty.data.Data.Identifier
+
+import threesixty.persistence.DatabaseAdapter
+
+import scala.collection.immutable.{Map => ImmutableMap}
+
+
+/**
+ *  This contains all datasets requested by the client.
+ *
+ *  @param dataIDs Set of IDs of datasets that will be processed.
+ *  @param databaseAdapter DatabaseAdapter
+ */
+@throws[NoSuchElementException]("if an id was given, for which no InputData exists.")
+class DataPool(
+    val dataIDs: Set[Identifier],
+    implicit val databaseAdapter: DatabaseAdapter
+) {
+
+    require(dataIDs.size > 0, "Empty ID Set is not allowed.")
+
+    // get data from db
+    val inputDatasets: Set[InputData] =
+        dataIDs.map(databaseAdapter.getDataset(_) match {
+            case Right(data) => data
+            case Left(error) => throw new NoSuchElementException(error)
+        })
+
+    // convert input data to processed data
+    var processedDatasets: Map[Identifier, ProcessedData] =
+        (for { data <- inputDatasets} yield (data.id, data: ProcessedData)).toMap
+
+    /**
+     *  Inserts data into the processedDatasets Map
+     *  @param data Set of data to be added to processedDatasets
+     */
+    def pushData(data: Set[ProcessedData]): Unit = {
+        data.foreach {
+            d => processedDatasets += (d.id -> d)
+        }
+    }
+
+    /**
+     *  Accessor for processedDatasets
+     *  @return Immutable Map of [[threesixty.data.Data.Identifier]] -> [[threesixty.data.ProcessedData]]
+     */
+    def datasets: ImmutableMap[Identifier, ProcessedData] = processedDatasets
+
+
+    @throws[NoSuchElementException]("if a dataset was requested that is not in processedDatasets")
+    def getDatasets(ids: Set[Identifier]): Set[ProcessedData] =
+        ids.map(processedDatasets(_))
+
+    @throws[NoSuchElementException]("if a dataset was requested that is not in processedDatasets")
+    def getDataset(id: Identifier): ProcessedData =
+        processedDatasets(id)
+
+}
+
