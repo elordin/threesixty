fee69abbb62b83300e9474da554759c67c2cb5a8
MarkusSchnappi <markus.schnappinger@gmx.de>
2016-01-30 15:36:06 +0100

    VisDeduct: Algorithm in DataRequirement
    refactored ( DataPool -> ProcessingStep; ambiguous naming -> dedicated
    naming)
    
    added implementation for excluded and demanded ProcessingMethods

--- src/main/scala/threesixty/visualizer/VisualizationMetadata.scala
+++ src/main/scala/threesixty/visualizer/VisualizationMetadata.scala
@@ -3,14 +3,14 @@ package threesixty.visualizer
 /**
   * @author Thomas Engel
   */
-case class VisualizationMetadata(val dataRequirement: List[DataRequirement],
+case class VisualizationMetadata(val requirementList: List[DataRequirement],
                                  val unlimitedData: Boolean = false) {
 
-    require(dataRequirement != null && dataRequirement.size > 0, "There must be at least one data required")
+    require(requirementList != null && requirementList.size > 0, "There must be at least one data required")
 
     def numberOfInputs(): Int = unlimitedData match {
         case true => Int.MaxValue
-        case false => dataRequirement.size
+        case false => requirementList.size
     }
 }
 
