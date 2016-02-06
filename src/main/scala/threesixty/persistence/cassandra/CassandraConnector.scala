06458d1b56558b24b14ec479ed3e4be3d7c32c1d
Thomas Engel <thomas.engel@tum.de>
2016-02-06 18:21:50 +0100

    Ip and keyspace is not read from the config
    Since Thomas Weber mentioned that it is preferable to load the
    information from the config, I changed the implementation according to
    that.

--- src/main/scala/threesixty/persistence/cassandra/CassandraConnector.scala
+++ src/main/scala/threesixty/persistence/cassandra/CassandraConnector.scala
@@ -1,7 +1,7 @@
 package threesixty.persistence.cassandra
 
+import com.typesafe.config.{ConfigException, ConfigFactory, Config}
 import com.websudos.phantom.connectors.{ContactPoints, KeySpace}
-import threesixty.server.APIHandler
 
 /**
   * Created by Stefan Cimander on 19.01.16.
@@ -11,10 +11,18 @@ trait CassandraKeyspace {
 }
 
 object CassandraConnector extends CassandraKeyspace {
+    val config: Config = ConfigFactory.load
+
+    @throws[ConfigException]("if config doesn't contain database.address") // TODO
+    val dbAddress: String =
+        config.getString("database.address")
+    @throws[ConfigException]("if config doesn't contain database.keyspace") // TODO
+    val dbKeyspace: String =
+        config.getString("database.keyspace")
+
     // val hosts = Seq("137.250.170.136")
     // val hosts = Seq("localhost")
-    val hosts = Seq(APIHandler.dbAddress)
-    val dbKeyspace = APIHandler.dbKeyspace
+    val hosts = Seq(dbAddress)
 
     val keyspace = ContactPoints(hosts).keySpace(dbKeyspace)
 }
